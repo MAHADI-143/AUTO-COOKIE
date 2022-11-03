fee254edd19e2f9937cd101e754f0ffa8351bd6f
MAHADI HASAN AFRIDI <79738922+MAHADI-143@users.noreply.github.com>
2022-11-03 21:32:03 +0600

    Update M-COOKIE.py

--- M-COOKIE.py
+++ M-COOKIE.py
@@ -16,7 +16,7 @@ bit = platform.architecture()[0]
  
 if bit == "64bit":
  
-        from c import main
+        from COOKIE64 import main
  
         main()
  
@@ -24,7 +24,7 @@ if bit == "64bit":
  
 elif bit == "32bit":
  
-        from cookies import main
+        from COOKIE32 import main
  
  
         main()
